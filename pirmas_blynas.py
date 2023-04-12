43a10f448b44257701d3c25fec55f628914df27b
AndriusG <agedvilasplay@gmail.com>
2023-04-12 16:30:13 +0300

    andrius

---
+++ pirmas_blynas.py
@@ -0,0 +1,9 @@
+tekstas = "Labas pasauli"
+if tekstas.find("pasauli"):
+    print("radau pasauli")
+else:
+    print("neradau pasauli")
+if tekstas.find("Labas"):
+    print("radau Labas")
+else:
+    print("neradau Labas")
