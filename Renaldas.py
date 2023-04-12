f9ab00f08e1e73c032d41918a93c2e492d8a13b3
RenaldasZ <spikeris2@gmail.com>
2023-04-12 20:13:24 +0300

    pakeiciau viena zodi

--- Renaldas.py
+++ Renaldas.py
@@ -7,6 +7,6 @@ hobis = input("koks tavo mėgstamiausias hobis? ")
 
 print("Tavo vardas " + vardas)
 print("Tu esi " + metai + " metų")
-print("Tavo mėgstamiausia kalba " + spalva)
+print("Tavo mėgstamiausia spalva " + spalva)
 print("Jei būtum gyvūnas koks būtum? " + gyvunas)
 print("Tavo pagrindinis hobis yra " + hobis)
