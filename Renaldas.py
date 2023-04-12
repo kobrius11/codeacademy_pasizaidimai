42beeaa977f47eefd271de88acbc87ee00086564
RenaldasZ <spikeris2@gmail.com>
2023-04-12 16:25:05 +0300

    renaldas

---
+++ Renaldas.py
@@ -0,0 +1,12 @@
+vardas = input("Koks tavo vardas? ")
+metai = input("Kiek tau metų? ")
+spalva = input("kokia tavo mėgstamiausia spalva? ")
+gyvunas = input("Jei būtum gyvūnas koks būtum? ")
+hobis = input("koks tavo mėgstamiausias hobis? ")
+
+
+print("Tavo vardas " + vardas)
+print("Tu esi " + metai + " metų")
+print("Tavo mėgstamiausia kalba " + spalva)
+print("Jei būtum gyvūnas koks būtum? " + gyvunas)
+print("Tavo pagrindinis hobis yra " + hobis)
