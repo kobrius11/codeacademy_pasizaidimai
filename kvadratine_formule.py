e02c9df282b32f9ec4e7a5a886b8ae02d1a6ca28
Kobrius11 <kobrius11@gmail.com>
2023-04-13 12:01:16 +0300

    prideta komplexinio sprendimo logika

--- kvadratine_formule.py
+++ kvadratine_formule.py
@@ -13,25 +13,30 @@ def kvadratine_formule():
         x2 = None
     
         #diskriminantas = b**2 - 4ac
-        diskriminantas = (b**2) - 4*(a*c)
+        diskriminantas = (b ** 2) - 4 * (a * c)
 
         if diskriminantas == 0:
-            x1 = (-b )/ (2*a)
-            return f"x1 = x2: {x1: .1f}, nes diskriminatas = {diskriminantas}"
+            x1 = (-b )/ (2 * a)
+            return f"x1 = x2:\n {x1: .1f}\n nes diskriminatas = {diskriminantas}"
 
 
         elif diskriminantas > 0:
-            x1 = (-b - (diskriminantas**(1/2))) / 2*a
-            x2 = (-b + (diskriminantas**(1/2))) / 2*a
+            x1 = (-b - (diskriminantas ** (1/2))) / 2 * a
+            x2 = (-b + (diskriminantas ** (1/2))) / 2 * a
 
-            return f"x1 != x2: x1={x1: .1f}, x2={x2: .1f}, nes diskriminatas = {diskriminantas}"
+            return f"x1 != x2:\n x1={x1: .1f}\n x2={x2: .1f}\n nes diskriminatas = {diskriminantas}"
 
         elif diskriminantas < 0:
-            return f'sprendiniu realiuju skaiciu aibeje nera, nes diskriminantas = {diskriminantas}'
+
+            x1 = complex((-b), - ((-1 * diskriminantas) ** (1/2)))
+            x2 = complex((-b), + ((-1 * diskriminantas) ** (1/2)))
+            #print(f"{x1 / (2* a): .4f}, {x2 / (2* a): .4f}")
+
+            return f'sprendiniu realiuju skaiciu aibeje nera, nes diskriminantas = {diskriminantas}, kompleksiniai sprendimai:\n x1 = {x1 / (2* a): .4f}\n x2 = {x2 / (2* a): .4f}'
     
 
 print(kvadratine_formule()) 
 
 # tolesni veiksmai:
-# padaryt kad sprestu D < 0 (kompleksines) lygtis
+# padaryt kad sprestu D < 0 (kompleksines) lygtis - baigta
 # padaryt kad funkcija priimtu argumentus, jei argumentu nera prasytu ivesti input 
